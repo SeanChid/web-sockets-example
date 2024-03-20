b315d7525b812100229625b10110a141c5fb04ac
SeanChid <seanchidester737@gmail.com>
2024-03-19 19:01:19 -0600

    fixed leave chat message

--- server/websocket.js
+++ server/websocket.js
@@ -7,7 +7,7 @@ const clientsByLobby = new Map()
 
 const broadcast = (lobbyId, message, excludeClient) => {
     wss.clients.forEach(client => {
-        if (client !== excludeClient && clientsByLobby.get(client) === lobbyId && client.readyState === WebSocket.OPEN) {
+        if (client !== excludeClient && clientsByLobby.get(client).lobbyId === lobbyId && client.readyState === WebSocket.OPEN) {
             client.send(message)
         }
     })
@@ -21,18 +21,17 @@ wss.on('connection', function connection(ws) {
         console.log(data)
 
         if (data.type === 'joinLobby') {
-            const lobbyId = data.lobbyId
 
-            clientsByLobby.set(ws, lobbyId)
+            clientsByLobby.set(ws, { lobbyId: data.lobbyId, userName: data.userName })
 
             const joinMessage = {
                 type: 'systemMessage',
                 message: `${data.userName} has joined the chat.`
             }
 
-            broadcast(lobbyId, JSON.stringify(joinMessage), ws)
+            broadcast(data.lobbyId, JSON.stringify(joinMessage), ws)
 
-            Message.findAll({where: {lobbyId: lobbyId}})
+            Message.findAll({where: {lobbyId: data.lobbyId}})
                 .then(messages => {
                     messages.forEach(message => {
                         ws.send(JSON.stringify(message))
@@ -44,7 +43,7 @@ wss.on('connection', function connection(ws) {
         }
 
         if (data.type === 'chatMessage') {
-            const lobbyId = clientsByLobby.get(ws)
+            const lobbyId = clientsByLobby.get(ws).lobbyId
 
             if (lobbyId) {
                 Message.create({
@@ -57,7 +56,7 @@ wss.on('connection', function connection(ws) {
                 })
                 .then(newMessage => {
                     wss.clients.forEach(client => {
-                        if (client.readyState === WebSocket.OPEN && clientsByLobby.get(client) === lobbyId) {
+                        if (client.readyState === WebSocket.OPEN && clientsByLobby.get(client).lobbyId === lobbyId) {
                             client.send(JSON.stringify(newMessage))
                         }
                     })
@@ -72,15 +71,16 @@ wss.on('connection', function connection(ws) {
     ws.on('close', function close() {
         console.log('A client has disconnected from the chat.')
 
-        const lobbyId = clientsByLobby.get(ws)
-        if (lobbyId) {
-            clientsByLobby.delete(ws)
-
+        const userData = clientsByLobby.get(ws)
+        if (userData) {
+            const {lobbyId, userName} = userData
+            
             const leaveMessage = {
                 type: 'systemMessage',
-                message: 'A client has left the chat.'
+                message: `${userName} has left the chat.`
             }
             broadcast(lobbyId, JSON.stringify(leaveMessage), ws)
+            clientsByLobby.delete(ws)
         }
 
     })
